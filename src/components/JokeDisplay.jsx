1c651f8bd7b79bdc98171c210fb29ad303d7baf8
Michael McCormack <mcctwin@gmail.com>
2025-08-05 13:48:34 -0500

    2 tests passed, 1 failed

--- src/components/JokeDisplay.jsx
+++ src/components/JokeDisplay.jsx
@@ -1,10 +1,17 @@
 // Step 1: Accept `joke` and `loading` as props
 
 const JokeDisplay = ({ joke, loading }) => {
+  if (loading) {
+    return <div className="joke-container">Loading...</div>
+  }
+
   return (
-    <div className="joke-container">
-      {/* Step 2: If `loading` is true, display "Loading..." */}
-      {/* Step 3: Otherwise, display the joke */}
+    <div className="joke-container" >
+      {loading ? (
+        <div>Loading...</div>
+      ) : (
+        <p>{joke}</p>
+      )}
     </div>
   )
 }
